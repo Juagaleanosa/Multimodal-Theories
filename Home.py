7a52b867f68ce85e0b28b63efeae09a719bb4c1b
Perceptrón Ai <93463327+Juagaleanosa@users.noreply.github.com>
2024-03-27 11:51:27 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -102,7 +102,7 @@ else:
     
 # Agrega una caja con el texto deseado
 st.markdown("""
-    <div style="background-color: #f0f0f0; padding: 10px; border-radius: 10px;">
+    <div style="background-color: #161B22; padding: 8px; border-radius: 10px;">
         <p style="text-align: center;">
             Dávila, M. V., Tamayo, A. Ó. E., Galeano, S. J. A. (2024). Philodatagrapher (versión 1) [Aplicación móvil]. Streamlit.app. <a href="https://philodatagrapher.streamlit.app">https://philodatagrapher.streamlit.app</a>.
         </p>
